45b10b48bd0fed284cf70ffa9ea101cf8787edbe
motaher13 <motaher1333@gmail.com>
2019-03-11 17:59:50 +0600

    export added

--- resources/views/result/batch.blade.php
+++ resources/views/result/batch.blade.php
@@ -20,10 +20,15 @@
                         {{--</div>--}}
                         <div class="table-toolbar">
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-10">
                                     <div class="caption caption-md">
                                         <i class="icon-globe theme-font hide"></i>
                                         <span class="caption-subject font-blue-madison bold uppercase">Rank List</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="caption caption-md">
+                                        <button onclick="exportTableToExcel('tblData', 'members-data')" class="btn btn-success">Export</button>
                                     </div>
                                 </div>
                                 {{--<div class="col-md-6">--}}
@@ -38,41 +43,42 @@
                         {{--</div>--}}
                         <div class="portlet-body">
                             <div class="tab-content">
+
                                 <!-- PERSONAL INFO TAB -->
                                 <div class="tab-pane active" >
                                     <div class="table-responsive">
-                                        <table class="table table-striped table-hover">
+                                        <table class="table table-striped table-hover tr" id="tblData">
                                             <thead>
                                             <tr>
                                                 <th >Rank</th>
                                                 <th >Name</th>
                                                 <th >Registration No</th>
                                                 <th >Marks</th>
-                                                <th></th>
+                                                {{-- <th> Actions </th> --}}
                                             </tr>
                                             </thead>
                                             <tbody >
                                             <?php $i=0;?>
                                             @foreach($answers as $answer)
                                                 <?php $i++?>
-                                                <tr class='clickable-row' data-href='#'>
-                                                    <td><h4>{{$i}}</h4></td>
-                                                    <td><h4>{{$answer->student->username}}</h4></td>
-                                                    <td><h4>{{$answer->student->userinfo->reg_no}}</h4></td>
-                                                    <td><h4>{{$answer->marks}}</h4></td>
-                                                    <td><a class="btn sbold green btn-xs" href="{{ route('exam.sheetMarks',$answer->id) }}" >View Answer </a></td>
+                                                <tr>
+                                                    <td style="text-align:center">{{$i}}</td>
+                                                    <td style="text-align:center">{{$answer->student->username}}</td>
+                                                    <td style="text-align:center">{{$answer->student->userinfo->reg_no}}</td>
+                                                    <td style="text-align:center">{{$answer->marks}}</td>
+                                                    <td id="myRow"><a class="btn sbold green btn-xs" href="{{ route('exam.sheetMarks',$answer->id) }}" >View Answer </a></td>
                                                     {{--<td>--}}
-                                                        {{--<div class="media">--}}
-                                                            {{--<div class="media-body">--}}
-                                                                {{--<span class="media-meta pull-right">{{$answer->exam->course->course_code}}</span>--}}
-                                                                {{--<h4 class="title">--}}
-                                                                    {{--. {{$answer->student->userinfo->reg_no}}--}}
-                                                                    {{--<span class="pull-right pagado">{{$answer->exam->course->title}}</span>--}}
-                                                                {{--</h4>--}}
-                                                                {{--<span class="media-meta pull-right">{{$answer->end}}</span>--}}
-                                                                {{--<p class="summary">Marks: {{$answer->marks}}</p>--}}
-                                                            {{--</div>--}}
-                                                        {{--</div>--}}
+                                                    {{--<div class="media">--}}
+                                                    {{--<div class="media-body">--}}
+                                                    {{--<span class="media-meta pull-right">{{$answer->exam->course->course_code}}</span>--}}
+                                                    {{--<h4 class="title">--}}
+                                                    {{--. {{$answer->student->userinfo->reg_no}}--}}
+                                                    {{--<span class="pull-right pagado">{{$answer->exam->course->title}}</span>--}}
+                                                    {{--</h4>--}}
+                                                    {{--<span class="media-meta pull-right">{{$answer->end}}</span>--}}
+                                                    {{--<p class="summary">Marks: {{$answer->marks}}</p>--}}
+                                                    {{--</div>--}}
+                                                    {{--</div>--}}
                                                     {{--</td>--}}
                                                 </tr>
                                             @endforeach
@@ -82,6 +88,7 @@
 
                                 </div>
                                 <!-- END PERSONAL INFO TAB -->
+                                <script src="{{asset('assets/js/app.js')}}"></script>
 
                             </div>
                         </div>
@@ -91,7 +98,7 @@
         </div>
         <!-- END PROFILE CONTENT -->
 
-@endsection
+        @endsection
 
         @section('scripts')
             <script>
@@ -104,33 +111,33 @@
             </script>
         @endsection
 
-@section('styles')
-    <style>
-        .media-meta {
-            font-size: 11px;
-            color: #999;
-        }
-        h4 {
+        @section('styles')
+            <style>
+                .media-meta {
+                    font-size: 11px;
+                    color: #999;
+                }
+                h4 {
 
-            font-size: 18px;
-            font-weight: bold;
-            line-height: normal;
-            margin-bottom: 5px;
-        }
-        .media .title span {
-            font-size: .8em;
-            margin-right: 20px;
-        }
-        .media .title span.pagado {
-            color: #5cb85c;
-        }
-        .media .summary {
-            font-size: 16px;
-            font-weight: bold;
-        }
-        p{
-            margin: 0px;
-        }
-    </style>
+                    font-size: 18px;
+                    font-weight: bold;
+                    line-height: normal;
+                    margin-bottom: 5px;
+                }
+                .media .title span {
+                    font-size: .8em;
+                    margin-right: 20px;
+                }
+                .media .title span.pagado {
+                    color: #5cb85c;
+                }
+                .media .summary {
+                    font-size: 16px;
+                    font-weight: bold;
+                }
+                p{
+                    margin: 0px;
+                }
+            </style>
 
 @endsection
